ffffd605240c02fabe1fcdb4e55442ad08d86438
Patrick Gerbes <patrickgerbes@Patricks-MacBook-Pro-2.local>
2016-12-12 13:30:40 -0500

    Add method to mongodb controller to load exchange reports by date

--- app/controllers/MongodbController.scala
+++ app/controllers/MongodbController.scala
@@ -2,7 +2,8 @@ package controllers
 
 import com.typesafe.config.{Config, ConfigFactory}
 import javax.inject.Inject
-import models.domain.storj.{Email, User}
+
+import models.domain.storj.{Email, ExchangeReport, User}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc.{Action, AnyContent, Controller}
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
@@ -10,6 +11,7 @@ import reactivemongo.api.collections.bson.BSONCollection
 import reactivemongo.api.{DefaultDB, MongoConnection, MongoDriver}
 import reactivemongo.api.MongoConnection.ParsedURI
 import reactivemongo.bson.{BSONDocument, BSONDocumentReader, Macros}
+
 import scala.concurrent.Future
 import scala.util.Try
 
@@ -25,7 +27,9 @@ class MongodbController @Inject()(val reactiveMongoApi: ReactiveMongoApi) extend
   val mConnection: Try[MongoConnection] = parsedUri.map(driver.connection(_))
   val futureConnection: Future[MongoConnection] = Future.fromTry(mConnection)
   val bridgeDb: Future[DefaultDB] = futureConnection.flatMap(_.database("bridge"))
+
   val usersCollection: Future[BSONCollection] = bridgeDb.map(_.collection("users"))
+  val exchangeReportsCollection: Future[BSONCollection] = bridgeDb.map(_.collection("exchangereports"))
 
   implicit val emailReader: BSONDocumentReader[Email] = Macros.reader[Email]
   implicit val userReader: BSONDocumentReader[User] = Macros.reader[User]
@@ -36,5 +40,13 @@ class MongodbController @Inject()(val reactiveMongoApi: ReactiveMongoApi) extend
     docs.find(findById).cursor[User].collect[Vector]()
     }
     data.map(x => Ok(x.toString))
+  }
+
+  def loadExchangeReports(date: String): Action[AnyContent] = Action.async{
+    val filterByDate = BSONDocument("created" -> date)
+    val reportData = exchangeReportsCollection.map { docs =>
+      docs.find(filterByDate).cursor[ExchangeReport].collect[Vector]()
+    }
+    reportData.map(er => Ok(er.toString))
   }
 }
