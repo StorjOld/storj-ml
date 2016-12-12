477f1979fa77bde487ddcd7da4c394e6720b54d4
Patrick Gerbes <patrickgerbes@Patricks-MacBook-Pro-2.local>
2016-12-12 13:14:11 -0500

    Clean up imports and types

--- app/controllers/MongodbController.scala
+++ app/controllers/MongodbController.scala
@@ -1,36 +1,36 @@
 package controllers
 
+import com.typesafe.config.{Config, ConfigFactory}
 import javax.inject.Inject
-
-import com.typesafe.config.ConfigFactory
 import models.domain.storj.{Email, User}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, AnyContent, Controller}
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import reactivemongo.api.collections.bson.BSONCollection
 import reactivemongo.api.{DefaultDB, MongoConnection, MongoDriver}
+import reactivemongo.api.MongoConnection.ParsedURI
 import reactivemongo.bson.{BSONDocument, BSONDocumentReader, Macros}
-
 import scala.concurrent.Future
+import scala.util.Try
 
 class MongodbController @Inject()(val reactiveMongoApi: ReactiveMongoApi) extends Controller
   with MongoController
   with ReactiveMongoComponents {
 
-  val mongoConfig = ConfigFactory.load().getConfig("mongodb")
-  val mongoUri = mongoConfig.getString("uri")
+  val mongoConfig: Config = ConfigFactory.load().getConfig("mongodb")
+  val mongoUri: String = mongoConfig.getString("uri")
 
-  val mdriver = MongoDriver()
-  val parsedUri = MongoConnection.parseURI(mongoUri)
-  val mConnection = parsedUri.map(driver.connection(_))
-  val futureConnection = Future.fromTry(mConnection)
+  val mongoDriver = MongoDriver()
+  val parsedUri: Try[ParsedURI] = MongoConnection.parseURI(mongoUri)
+  val mConnection: Try[MongoConnection] = parsedUri.map(driver.connection(_))
+  val futureConnection: Future[MongoConnection] = Future.fromTry(mConnection)
   val bridgeDb: Future[DefaultDB] = futureConnection.flatMap(_.database("bridge"))
   val usersCollection: Future[BSONCollection] = bridgeDb.map(_.collection("users"))
 
   implicit val emailReader: BSONDocumentReader[Email] = Macros.reader[Email]
   implicit val userReader: BSONDocumentReader[User] = Macros.reader[User]
 
-  def findUserByEmail(id: String) = Action.async{
+  def findUserByEmail(id: String): Action[AnyContent] = Action.async{
     val findById = BSONDocument("id" -> id)
     val data = usersCollection.map{ docs =>
     docs.find(findById).cursor[User].collect[Vector]()
